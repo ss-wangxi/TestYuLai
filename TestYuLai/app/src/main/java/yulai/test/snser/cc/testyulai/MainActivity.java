1188d84ce47be90a900e7d5111763d6ab8e095a9
snser <snsermail@gmail.com>
2017-04-05 16:35:41 +0800

    init

---
+++ TestYuLai/app/src/main/java/yulai/test/snser/cc/testyulai/MainActivity.java
@@ -0,0 +1,14 @@
+package yulai.test.snser.cc.testyulai;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
